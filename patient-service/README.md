a3d56971949f9bebdb8187f173828993d71d38c5
nick <wk@kucharski.ovh>
2025-08-14 13:48:01 +0200

    Fixed so github has all modules instead of just patient-service

---
+++ patient-service/README.md
@@ -0,0 +1,1 @@
+Production-Ready Patient Managements System with Microservices, Spring Boot, Kafka, Docker and AWS.
---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,33 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+
+        PatientResponseDTO dto = new PatientResponseDTO();
+        dto.setId(patient.getId().toString());
+        dto.setName(patient.getName());
+        dto.setEmail(patient.getEmail());
+        dto.setAddress(patient.getAddress());
+        dto.setDateOfBirth(patient.getBirthDate().toString());
+
+        return dto;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setBirthDate(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        return patient;
+    }
+}
