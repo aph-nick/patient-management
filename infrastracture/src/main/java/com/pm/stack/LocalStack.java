859fa297f435272868d98ae8aaab7bec47e6a0dc
nick <wk@kucharski.ovh>
2025-08-24 00:23:19 +0200

    Added Databases HealthCheck

--- infrastracture/src/main/java/com/pm/stack/LocalStack.java
+++ infrastracture/src/main/java/com/pm/stack/LocalStack.java
@@ -6,6 +6,8 @@ import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
+import software.amazon.awscdk.services.route53.CfnHealthCheckProps;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -16,8 +18,12 @@ public class LocalStack extends Stack {
         this.vpc = createVpc();
 
         DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+        CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDbHealthCheck");
 
         DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service--db");
+        CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDbHealthCheck");
+
+
     }
 
     private Vpc createVpc(){
@@ -42,6 +48,18 @@ public class LocalStack extends Stack {
                 .credentials(Credentials.fromGeneratedSecret("admin_user"))
                 .databaseName(dbName)
                 .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance db, String id){
+        return CfnHealthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP")
+                        .port(Token.asNumber(db.getDbInstanceEndpointPort()))
+                        .ipAddress(db.getDbInstanceEndpointAddress())
+                        .requestInterval(30)
+                        .failureThreshold(3)
+                        .build())
                 .build();
     }
 
